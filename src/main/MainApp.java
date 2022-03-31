b002a84eb3bd264866637fbda5e353015e002701
Babar Hussain <hussainbabarkh@gmail.com>
2022-03-31 07:45:46 +0500

    added

--- src/main/MainApp.java
+++ src/main/MainApp.java
@@ -3,7 +3,7 @@ package main;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import dto.DeveloperDTO;
+import emp.Employee;
 
 public class MainApp {
 	public static void main(String[] args) {
@@ -12,10 +12,18 @@ public class MainApp {
 //		BeanFactory factory = new XmlBeanFactory(resource);
 //		DeveloperDTO obj = (DeveloperDTO) factory.getBean("dev1");
 
-		ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
-		DeveloperDTO obj = (DeveloperDTO) context.getBean("dev1");
+//		ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
+//		DeveloperDTO obj = (DeveloperDTO) context.getBean("dev1");
+//
+//		System.out.println(obj);
 
-		System.out.println(obj);
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("application.xml");
+
+		EmployeeDao dao = (EmployeeDao) ctx.getBean("edao");
+//		int status = dao.saveEmployee(new Employee(102, "Amit", 35000));
+//		System.out.println(status);
+		
+		dao.g
 
 	}
 }
--- src/quiz/QuizApp.java
+++ src/quiz/QuizApp.java
@@ -1,11 +1,14 @@
 package quiz;
 
+import java.util.Map;
+import java.util.Scanner;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import customer.CustomerBo;
+import dto.Quiz;
 
 @Configuration
 @EnableAspectJAutoProxy
@@ -15,31 +18,31 @@ public class QuizApp {
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 
-//		Scanner inp = new Scanner(System.in);
+		Scanner inp = new Scanner(System.in);
 //
 		ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
-//		Quiz obj = (Quiz) context.getBean("quiz1");
-//
-//		System.out.println(obj.getQuestions());
-//		System.out.println(obj.getAnswers());
-//
-//		Map anserAnswers = obj.getAnswers();
-//		obj.getQuestions().forEach((q) -> {
-//
-//			System.out.println(q.getId() + ": " + q.getTitle());
-//			System.out.println("\t A." + q.getOptions().get(0));
-//			System.out.println("\t B." + q.getOptions().get(1));
-//			System.out.println("\t C." + q.getOptions().get(2));
-//
-//			System.out.print("Answer:");
-//			String answer = inp.nextLine();
-//
-//			if (anserAnswers.get(q.getId()).equals(answer)) {
-//				correct++;
-//			}
-//		});
-//		System.out.println("Correct:" + correct);
-//		System.out.println("End");
+		Quiz obj = (Quiz) context.getBean("quiz1");
+
+		System.out.println(obj.getQuestions());
+		System.out.println(obj.getAnswers());
+
+		Map anserAnswers = obj.getAnswers();
+		obj.getQuestions().forEach((q) -> {
+
+			System.out.println(q.getId() + ": " + q.getTitle());
+			System.out.println("\t A." + q.getOptions().get(0));
+			System.out.println("\t B." + q.getOptions().get(1));
+			System.out.println("\t C." + q.getOptions().get(2));
+
+			System.out.print("Answer:");
+			String answer = inp.nextLine();
+
+			if (anserAnswers.get(q.getId()).equals(answer)) {
+				correct++;
+			}
+		});
+		System.out.println("Correct:" + correct);
+		System.out.println("End");
 
 //		A a = (A) context.getBean("proxy");
 //		a.m();
@@ -48,8 +51,8 @@ public class QuizApp {
 //		  
 //	        manager.getEmployeeById(1);
 
-		CustomerBo customer = (CustomerBo) context.getBean("customerBo");
-		customer.addCustomer();
+//		CustomerBo customer = (CustomerBo) context.getBean("customerBo");
+//		customer.addCustomer();
 	}
 
 }
